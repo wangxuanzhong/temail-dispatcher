b19cf36a9d196552699bb802a52de929b730d474
姚华成 <yaohuacheng@syswin.com>
2018-08-31 17:20:15 +0800

    根据Channel Service服务的修改进行调整

---
+++ src/main/java/com/syswin/temail/dispatcher/notify/entity/TemailAccountLocation.java
@@ -0,0 +1,30 @@
+package com.syswin.temail.dispatcher.notify.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemailAccountLocation {
+
+  //账户信息
+  private String account;
+
+  // 移动端设备id
+  private String devId;
+
+  // 持有客户端链句柄的服务实例宿主机地址
+  private String hostOf;
+
+  // 持有客户端链句柄的服务实例的进程号
+  private String processId;
+
+  // 持有客户端链句柄的服务实例监听的消息队列topic
+  private String mqTopic;
+
+  // 持有客户端链句柄的服务实例监听的消息队列mqTag
+  private String mqTag;
+
+}
