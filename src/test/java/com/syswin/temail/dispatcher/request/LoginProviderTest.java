affe2774df048e11357e8674f3291ae8737deec5
姚华成 <yaohuacheng@syswin.com>
2018-09-03 19:51:39 +0800

    取消CDTPPacket对象

--- src/test/java/com/syswin/temail/dispatcher/request/LoginProviderTest.java
+++ src/test/java/com/syswin/temail/dispatcher/request/LoginProviderTest.java
@@ -4,7 +4,6 @@ import static com.seanyinx.github.unit.scaffolding.Randomness.uniquify;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
 
 import au.com.dius.pact.provider.junit.Provider;
@@ -15,12 +14,10 @@ import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import au.com.dius.pact.provider.spring.SpringRestPactRunner;
 import com.google.gson.Gson;
-import com.syswin.temail.dispatcher.entity.CDTPPacket;
 import com.syswin.temail.dispatcher.request.application.AuthService;
 import com.syswin.temail.dispatcher.request.application.PackageDispatcher;
 import com.syswin.temail.dispatcher.request.controller.Response;
 import com.syswin.temail.dispatcher.request.entity.CDTPPacketTrans;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import org.jetbrains.annotations.NotNull;
@@ -37,22 +34,19 @@ import org.springframework.web.client.RestClientException;
 @SpringBootTest(webEnvironment = DEFINED_PORT, properties = "server.port=8081")
 @ActiveProfiles("dev")
 public class LoginProviderTest {
+
+  private static final String ackMessage = "Sent ackMessage";
   @TestTarget
   public final Target target = new HttpTarget(8081);
   private final String unsignedBytes = "abc";
   private final String signature = "signed-abc";
-
-
   private final String sender = "jack@t.email";
   private final String receiver = "sean@t.email";
   private final String message = "hello world";
   private final String deviceId = uniquify("deviceId");
-
-  private static final String ackMessage = "Sent ackMessage";
-
   private final Gson gson = new Gson();
 
-  private final CDTPPacketTrans   cdtpPacketTrans = singleChatPacket(sender, receiver, message, deviceId);
+  private final CDTPPacketTrans cdtpPacketTrans = singleChatPacket(sender, receiver, message, deviceId);
 
 
   @MockBean
@@ -64,12 +58,14 @@ public class LoginProviderTest {
 
   @State("User sean is registered")
   public void userIsRegistered() {
-    when(authService.verify("sean@t.email", unsignedBytes, signature)).thenReturn(ResponseEntity.ok(Response.ok("Success")));
+    when(authService.verify("sean@t.email", unsignedBytes, signature))
+        .thenReturn(ResponseEntity.ok(Response.ok("Success")));
   }
 
   @State("User jack is not registered")
   public void userNotRegistered() {
-    when(authService.verify("jack@t.email", unsignedBytes, signature)).thenReturn(new ResponseEntity<>(Response.failed(FORBIDDEN), FORBIDDEN));
+    when(authService.verify("jack@t.email", unsignedBytes, signature))
+        .thenReturn(new ResponseEntity<>(Response.failed(FORBIDDEN), FORBIDDEN));
   }
 
   @State("User mike is registered, but server is out of work")
@@ -78,21 +74,22 @@ public class LoginProviderTest {
   }
 
   @State("dispatch user request")
-  public void   dispatchUserRequest() {
-    when(packageDispatcher.dispatch(cdtpPacketTrans)).thenReturn((ResponseEntity<String>)ResponseEntity.ok(gson.toJson(Response.ok(OK, ackPayload()))));
+  public void dispatchUserRequest() {
+    when(packageDispatcher.dispatch(cdtpPacketTrans))
+        .thenReturn((ResponseEntity<String>) ResponseEntity.ok(gson.toJson(Response.ok(OK, ackPayload()))));
 
   }
 
   // 创建单聊消息体
-  public  CDTPPacketTrans singleChatPacket(String sender, String recipient, String message, String deviceId) {
+  public CDTPPacketTrans singleChatPacket(String sender, String recipient, String message, String deviceId) {
 
-    CDTPPacketTrans cdtpPacketTrans  = new  CDTPPacketTrans();
+    CDTPPacketTrans cdtpPacketTrans = new CDTPPacketTrans();
     short CDTP_VERSION = 1;
     cdtpPacketTrans.setCommandSpace((short) 1);
-    cdtpPacketTrans.setCommand((short)1);
+    cdtpPacketTrans.setCommand((short) 1);
     cdtpPacketTrans.setVersion(CDTP_VERSION);
 
-    CDTPPacketTrans.Header   cdtpPacketTransHeader  = new  CDTPPacketTrans.Header();
+    CDTPPacketTrans.Header cdtpPacketTransHeader = new CDTPPacketTrans.Header();
     cdtpPacketTransHeader.setSignatureAlgorithm(1);
     cdtpPacketTransHeader.setSignature("sign");
     cdtpPacketTransHeader.setDataEncryptionMethod(0);
@@ -119,15 +116,12 @@ public class LoginProviderTest {
 
 
   @NotNull
-  private CDTPPacket ackPayload() {
+  private CDTPPacketTrans ackPayload() {
 
-    CDTPPacket payload = new CDTPPacket();
-    payload.setCommandSpace((short)1);
-    payload.setCommand((short)1);
-    payload.setData(gson.toJson(Response.ok(ackMessage)).getBytes());
+    CDTPPacketTrans payload = new CDTPPacketTrans();
+    payload.setCommandSpace((short) 1);
+    payload.setCommand((short) 1);
+    payload.setData(gson.toJson(Response.ok(ackMessage)));
     return payload;
   }
-
-
-
 }
