5c3d57cf5e90ddfc8d2272f8f1c62a0915f41b86
鞠爱华 <juaihua@syswin.com>
2018-11-20 14:56:18 +0800

    优化

--- src/main/java/com/syswin/temail/dispatcher/notify/NotificationMessageFactory.java
+++ src/main/java/com/syswin/temail/dispatcher/notify/NotificationMessageFactory.java
@@ -26,7 +26,8 @@ class NotificationMessageFactory {
 
   private final Gson gson;
 
-  private final Type type = new TypeToken<Map<String,Map<String,String>>>(){}.getType();
+  private final Type type = new TypeToken<Map<String, Map<String, String>>>() {
+  }.getType();
 
   NotificationMessageFactory() {
     gson = new Gson();
@@ -48,9 +49,9 @@ class NotificationMessageFactory {
     if (pushData.getEventType().equals(Constants.COMMON_MSG_EVENT_TYPE)) {
       PushMessage pushMsg = new PushMessage();
       BeanUtils.copyProperties(pushData, pushMsg);
-      Map<String,String> pushOptions = this.extractPushOptions(header);
-      pushMsg.setCmd(Optional.ofNullable(pushOptions.get("cmd")).orElse(null));
-      pushMsg.setType(Optional.ofNullable(pushOptions.get("type")).orElse(null));
+      Map<String, String> pushOptions = this.extractPushOptions(header);
+      pushMsg.setCmd(pushOptions.get("cmd"));
+      pushMsg.setType(pushOptions.get("type"));
       return Optional.ofNullable(gson.toJson(pushMsg));
     }
     return Optional.empty();
@@ -58,15 +59,15 @@ class NotificationMessageFactory {
 
   //extract push options
   Map<String, String> extractPushOptions(CDTPHeader header) {
-    Map<String, String> pushOptions = new HashMap<>();
     try {
       Map<String, Map<String, String>> extraOption = Optional.ofNullable(
-          gson.<Map<String, Map<String, String>>>fromJson(header.getExtraData(),type)).orElse(emptyMap());
-      pushOptions = extraOption.getOrDefault("push",emptyMap());
+          gson.<Map<String, Map<String, String>>>fromJson(header.getExtraData(), type)).orElse(emptyMap());
+      return extraOption.getOrDefault("push", emptyMap());
     } catch (JsonSyntaxException e) {
       log.error("从CDTPHeader中提取cmd 和 type 失败，extraData : {}", header.getExtraData(), e);
+      return emptyMap();
+
     }
-    return pushOptions;
   }
 
 }
