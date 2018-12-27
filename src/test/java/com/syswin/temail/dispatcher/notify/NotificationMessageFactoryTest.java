f5c322ed99712c08c8b159a8cf51107a82613b37
鞠爱华 <600852@syswin.com>
2018-12-27 19:26:27 +0800

    support to push offline message for eventType 17

--- src/test/java/com/syswin/temail/dispatcher/notify/NotificationMessageFactoryTest.java
+++ src/test/java/com/syswin/temail/dispatcher/notify/NotificationMessageFactoryTest.java
@@ -1,13 +1,9 @@
 package com.syswin.temail.dispatcher.notify;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import com.syswin.temail.dispatcher.Constants;
-import com.syswin.temail.dispatcher.notify.entity.PushData;
 import com.syswin.temail.ps.common.entity.CDTPHeader;
 import java.util.Map;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
 public class NotificationMessageFactoryTest {
 
@@ -60,20 +56,5 @@ public class NotificationMessageFactoryTest {
     pushOptions = notificationMessageFactory.extractPushOptions(cdtpHeader);
     return pushOptions;
   }
-
-  @Test
-  public void trueIfIsCommonMsg(){
-    PushData pushData = Mockito.mock(PushData.class);
-    Mockito.when(pushData.getEventType()).thenReturn(Constants.COMMON_MSG_EVENT_TYPE);
-    assertThat(notificationMessageFactory.judgeMessage(pushData)).isTrue();
-  }
-
-  @Test
-  public void trueIfIsNotracbMsg(){
-    PushData pushData = Mockito.mock(PushData.class);
-    Mockito.when(pushData.getEventType()).thenReturn(Constants.NOTRACE_MSG_EVENT_TYPE);
-    assertThat(notificationMessageFactory.judgeMessage(pushData)).isTrue();
-  }
-
 
 }
