33b7535d0656e67b0e9cebcf12a2767075d0f9d2
鞠爱华 <600852@syswin.com>
2019-02-23 15:00:55 +0800

    话题跳过验签

--- src/test/java/com/syswin/temail/dispatcher/codec/PacketTypeJudgeTest.java
+++ src/test/java/com/syswin/temail/dispatcher/codec/PacketTypeJudgeTest.java
@@ -19,9 +19,9 @@ public class PacketTypeJudgeTest {
     Assertions.assertThat(packetTypeJudge.isToBeVerifyRecieverTemail((short)1, (short)(0x1006))).isTrue();
     Assertions.assertThat(packetTypeJudge.isToBeVerifyRecieverTemail((short)1, (short)(0x100B))).isTrue();
 
-    Assertions.assertThat(packetTypeJudge.isToBeVerifyRecieverTemail((short)(0xE), (short)0x1)).isTrue();
-    Assertions.assertThat(packetTypeJudge.isToBeVerifyRecieverTemail((short)(0xE), (short)0x2)).isTrue();
-    Assertions.assertThat(packetTypeJudge.isToBeVerifyRecieverTemail((short)(0xE), (short)(0x4))).isTrue();
+    Assertions.assertThat(packetTypeJudge.isToBeVerifySkipped((short)(0xE), (short)0x1)).isTrue();
+    Assertions.assertThat(packetTypeJudge.isToBeVerifySkipped((short)(0xE), (short)0x2)).isTrue();
+    Assertions.assertThat(packetTypeJudge.isToBeVerifySkipped((short)(0xE), (short)(0x4))).isTrue();
 
   }
 
