0c31d2c739558f18312052d0f81b4d2d1f58bfcb
鞠爱华 <600852@syswin.com>
2019-03-18 22:22:45 +0800

    完成验签异步本地调用单元测试

--- src/main/java/com/syswin/temail/dispatcher/request/service/DispDispatcherService.java
+++ src/main/java/com/syswin/temail/dispatcher/request/service/DispDispatcherService.java
@@ -2,12 +2,11 @@ package com.syswin.temail.dispatcher.request.service;
 
 import com.syswin.temail.dispatcher.request.controller.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public interface DispDispatcherService {
 
-  public ResponseEntity<Response<String>> verify(@RequestBody byte[] payload) throws Exception ;
+  public ResponseEntity<Response<String>> verify( byte[] payload) throws Exception ;
 
-  public ResponseEntity<String> dispatch(@RequestBody byte[] payload) throws Exception ;
+  public ResponseEntity<String> dispatch( byte[] payload) throws Exception ;
 
 }
