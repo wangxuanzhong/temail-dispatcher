883b8a46666cc701873529e13fc70bfa4ee8d246
鞠爱华 <600852@syswin.com>
2019-07-31 10:18:50 +0800

    add code sync script

---
+++ sync2019Caller.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+# 如果有额外(即除.gitignore, .git外)的隐藏文件或者文件夹的情况，需要自行在util.sh增加中同步这些文件的操作。
+branch_name=$1
+commit_info=$2
+./sync2019Util.sh "git@172.31.68.105:temail-server2019/temail-dispatcher.git" "temail-dispatcher" "$branch_name" "$commit_info"
---
+++ sync2019Util.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+project_git=$1
+project_name=$2
+branch_name=$3
+commit_info=$4
+project_workir=sync-$2
+project_path=$(cd `dirname $0`; pwd)
+echo "server 2019 git location: $project_git"
+echo "server 2019 project name: $project_name"
+echo "server 2019 branch name: $branch_name"
+echo "server 2019 branch commit info: $commit_info"
+
+cd ~
+rm -rf $project_workir
+mkdir $project_workir
+cd $project_workir
+git clone $project_git
+cd $project_name
+git pull origin $branch_name
+
+rm -rf ./*
+rm -rf ./.gitignore
+
+cp -r $project_path/* ./
+cp $project_path/.gitignore ./
+rm -rf ./sync*.sh
+
+git add ./*
+echo "ready to commit with : $commit_info "
+git commit -m "$commit_info"
+
+git push origin $branch_name
+
+cd $project_path
+#rm -rf ~/$project_workir
