035bb7560509ccd692ce64b0011a174a9db807c4
刘兴 <liuxing@syswin.com>
2019-08-06 10:15:06 +0800

    change cross domain valid param name

--- src/main/java/com/syswin/temail/dispatcher/valid/params/ValidParamsCrossGroupFactory.java
+++ src/main/java/com/syswin/temail/dispatcher/valid/params/ValidParamsCrossGroupFactory.java
@@ -37,7 +37,7 @@ public class ValidParamsCrossGroupFactory implements ValidParamsFactory {
   public Optional<ValidParams> buildParams(CDTPPacket cdtpPacket,
       Function<CDTPPacket, String> signExtract) {
     Map<String, String> params = new HashMap<>();
-    params.put(temail, cdtpPacket.getHeader().getSender());
+    params.put(senderTemail, cdtpPacket.getHeader().getSender());
     params.put(receiverTemail,cdtpPacket.getHeader().getReceiver());
     return Optional.of(new ValidParams(PacketValidType.crossGroupsignValid.getAuthUri(),params));
   }
